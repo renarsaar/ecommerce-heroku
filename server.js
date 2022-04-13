9ec36c0d4c3cc2d708e3037127b570140fe5a80c
Renar Saaremets <58975004+renarsaar@users.noreply.github.com>
2022-04-13 12:51:16 +0300

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Production config
-app.use(express.static(path.join(__dirname, 'build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+
 
 app.use('/uploads', express.static('uploads'));
 
